eadda72d89d39763c5379295f84b89d392eb0cf0
NikitaG <nrg1@tpu.ru>
2022-01-27 05:20:15 +0700

    transmit with USART1

--- src/main.c
+++ src/main.c
@@ -1,7 +1,5 @@
 #include "stm32f407xx.h"
 
-volatile uint32_t time = 0;
-
 void wait(uint32_t ms)
 {
     SysTick->VAL = 0;
@@ -14,14 +12,44 @@ void wait(uint32_t ms)
     }
 }
 
+void transmit(uint8_t *data, uint32_t size)
+{
+    USART1->CR1 |= USART_CR1_TE;
+
+    for (int i = 0; i < size; i++)
+    {
+        while (!(USART1->SR & USART_SR_TXE))
+        {
+        }
+        USART1->DR = data[i];
+    }
+}
+
 int main()
 {
+    // GPIO and clock for LED2
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
     GPIOA->MODER |= GPIO_MODER_MODER6_0;
 
+    // GPIO and clock for USART1 (PA9 PA10)
+    RCC->APB2ENR |= RCC_APB2ENR_USART1EN_Msk;
+
+    GPIOA->MODER |= GPIO_MODER_MODE9_1;
+    GPIOA->AFR[1] |= GPIO_AFRH_AFSEL9_0 | GPIO_AFRH_AFSEL9_1 | GPIO_AFRH_AFSEL9_2;
+
+    // GPIOA->MODER |= GPIO_MODER_MODE10_1;
+    // GPIOA->AFR[1] |= GPIO_AFRH_AFSEL10_0 | GPIO_AFRH_AFSEL10_1 | GPIO_AFRH_AFSEL10_2;
+
+    // configuration for USART1
+    USART1->BRR = (uint16_t)(16000000 / 115200);
+    USART1->CR1 |= USART_CR1_UE_Msk;
+
     while (1)
     {
-        wait(1000);
+        wait(500);
+
+        uint8_t data[] = "some data\n";
+        transmit(data, sizeof(data) - 1);
         GPIOA->ODR ^= GPIO_ODR_ODR_6;
     }
 }
